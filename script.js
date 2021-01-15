ec615b91c4482d18e9c7bbeda054aa2f278f26e3
BramVanPevenage <55691795+BramVanPevenage@users.noreply.github.com>
2021-01-15 17:01:21 +0100

    update moving blocks

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 (function () {
   'use strict';
+  //global variabelen
   let birthDate = '12/12/2001';
   let procentNextAge = 0;
   let myAge = (calculateAge(birthDate));
+
+  // Event listeners
   window.addEventListener('load', () => {
-    
+
     document.getElementsByClassName('age')[0].innerHTML = Math.floor(myAge);
     document.getElementsByClassName('nextage')[0].innerHTML = Math.floor(myAge) + 1;
 
   });
+  document.addEventListener('mouseover', hoverProgressBar);
 
+  //functies
 
-
+  //bereken van leeftijd op een doorgegeven datum
   function calculateAge(birthDate) {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -30,7 +35,7 @@
 
     return myAge;
   }
-  document.addEventListener('mouseover', hoverProgressBar);
+
   function hoverProgressBar(e) {
     let tooltip = document.getElementsByClassName('tooltip')[0];
     let element = e.target;
@@ -38,22 +43,32 @@
     let y = e.clientY;
 
 
-    if(element.id == 'myAgeProgress' || element.id == 'myBar'){
+    if (element.id == 'myAgeProgress' || element.id == 'myBar') {
       tooltip.style.top = y - 20 + window.scrollY + 'px';
       tooltip.style.left = x + 15 + 'px';
-      tooltip.innerHTML = round(procentNextAge.replace('%',''), 2) + '%';
+      tooltip.innerHTML = round(procentNextAge.replace('%', ''), 2) + '%';
       tooltip.style.opacity = '100';
-    }
-    else{
+    } else if (element.id !== 'myAgeProgress' || element.id !== 'myBar' ) {
       tooltip.style.opacity = '0';
     }
-
-
+    if (element.className.includes('box') || element.parentElement.className.includes('box')) {
+      if (element.className.includes('box')) {
+        let coordinaten = element.getBoundingClientRect();
+        //element.style.position = 'absolute';
+        element.offsetLeft -= 5;
+      }
+      if (element.parentElement.className.includes('box')) {
+        let coordinaten = element.parentElement.getBoundingClientRect();
+        element.parentElement.style.position = 'absolute';
+        element.parentElement.style.top = coordinaten.x - 1 + 'px';
+        //element.parentElement.offsetLeft -= 5;
+      }
+    }
     
 
   }
 
   function round(value, decimals) {
-    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-  }  
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+  }
 })();
