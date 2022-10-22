df0c29e7d6674b25996a1183198b29b34c6007ad
Bram Van Pevenage <55691795+BramVanPevenage@users.noreply.github.com>
2022-10-22 12:26:52 +0200

    :recycle: javascript file formatting - prettier

--- script.js
+++ script.js
@@ -1,207 +1,208 @@
 (function () {
-  'use strict';
+  "use strict";
   //global variabelen
-  let birthDate = '12/12/2001';
+  let birthDate = "12/12/2001";
   let procentNextAge = 0;
-  let myAge = (calculateAge(birthDate));
+  let myAge = calculateAge(birthDate);
   let allBoxes = null;
 
   // Event listeners
-  window.addEventListener('load', () => {
-
-    document.getElementsByClassName('age')[0].innerHTML = Math.floor(myAge);
-    document.getElementsByClassName('nextage')[0].innerHTML = Math.floor(myAge) + 1;
+  window.addEventListener("load", () => {
+    document.getElementsByClassName("age")[0].innerHTML = Math.floor(myAge);
+    document.getElementsByClassName("nextage")[0].innerHTML =
+      Math.floor(myAge) + 1;
     aanmakenExitBtn();
 
     //box clickListeners => TODO: maak een functie die dit doet met behulp van een forloop, die alle objecten in een array doorloopt. met zelfde class.
-    document.getElementsByClassName('persoonlijk')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('persoonlijk')[0]);
+    document
+      .getElementsByClassName("persoonlijk")[0]
+      .addEventListener("click", () => {
+        selectBox(document.getElementsByClassName("persoonlijk")[0]);
+      });
+    document
+      .getElementsByClassName("ervaring")[0]
+      .addEventListener("click", () => {
+        selectBox(document.getElementsByClassName("ervaring")[0]);
+      });
+    document
+      .getElementsByClassName("projecten")[0]
+      .addEventListener("click", () => {
+        selectBox(document.getElementsByClassName("projecten")[0]);
+      });
+    document
+      .getElementsByClassName("opleiding")[0]
+      .addEventListener("click", () => {
+        selectBox(document.getElementsByClassName("opleiding")[0]);
+      });
+    document
+      .getElementsByClassName("hobbys")[0]
+      .addEventListener("click", () => {
+        selectBox(document.getElementsByClassName("hobbys")[0]);
+      });
+    document.getElementsByClassName("taal")[0].addEventListener("click", () => {
+      selectBox(document.getElementsByClassName("taal")[0]);
     });
-    document.getElementsByClassName('ervaring')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('ervaring')[0]);
-    });
-    document.getElementsByClassName('projecten')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('projecten')[0]);
-    });
-    document.getElementsByClassName('opleiding')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('opleiding')[0]);
-    });
-    document.getElementsByClassName('hobbys')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('hobbys')[0]);
-    });
-    document.getElementsByClassName('taal')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('taal')[0]);
-    });
-    document.getElementsByClassName('progervaring ')[0].addEventListener('click', () => {
-      selectBox(document.getElementsByClassName('progervaring ')[0]);
-    });
-    document.getElementsByClassName('btnExit')[0].addEventListener('click', () => {
-      BackToHomeScreen();
-      document.getElementsByClassName('btnExit ')[0].style.display = 'none';
-    });
-    document.addEventListener('mouseover', hoverProgressBar);
-
+    document
+      .getElementsByClassName("progervaring ")[0]
+      .addEventListener("click", () => {
+        selectBox(document.getElementsByClassName("progervaring ")[0]);
+      });
+    document
+      .getElementsByClassName("btnExit")[0]
+      .addEventListener("click", () => {
+        BackToHomeScreen();
+        document.getElementsByClassName("btnExit ")[0].style.display = "none";
+      });
+    document.addEventListener("mouseover", hoverProgressBar);
   });
-
-
-
-
 
   //functies
 
   //verwijderen van alle boxes tonen van inhoud van geselecteerde box, exit button aanmaken
 
   function selectBox(selectedBox) {
-    selectedBox.style.backgroundColor = 'blue';
-    selectedBox.style.margin = '0';
-    selectedBox.style.animation = 'none';
-    selectedBox.style.width = '100vw';
-    selectedBox.style.height = '100vh';
-    selectedBox.style.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
+    selectedBox.style.backgroundColor = "blue";
+    selectedBox.style.margin = "0";
+    selectedBox.style.animation = "none";
+    selectedBox.style.width = "100vw";
+    selectedBox.style.height = "100vh";
+    selectedBox.style.transform =
+      "perspective(1000px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)";
 
-    allBoxes = document.getElementsByClassName('box');
+    allBoxes = document.getElementsByClassName("box");
 
     for (let index = 0; index < allBoxes.length; index++) {
       if (allBoxes[index] != selectedBox) {
-        allBoxes[index].style.display = 'none';
+        allBoxes[index].style.display = "none";
       } else {
         for (let i = 0; i < allBoxes[index].children.length; i++) {
-          allBoxes[index].children[i].style.display = 'block';
-          if (allBoxes[index].children[i].tagName == 'H2') {
-            allBoxes[index].children[i].style.fontSize = '2.5rem';
+          allBoxes[index].children[i].style.display = "block";
+          if (allBoxes[index].children[i].tagName == "H2") {
+            allBoxes[index].children[i].style.fontSize = "2.5rem";
 
-            if (allBoxes[index].className.includes('persoonlijk')) {
-              allBoxes[index].children[i].style.fontSize = '18rem';
+            if (allBoxes[index].className.includes("persoonlijk")) {
+              allBoxes[index].children[i].style.fontSize = "18rem";
             }
-            if (allBoxes[index].className.includes('taal')) {
-              allBoxes[index].children[i].style.fontSize = '40rem';
+            if (allBoxes[index].className.includes("taal")) {
+              allBoxes[index].children[i].style.fontSize = "40rem";
             }
-            if (allBoxes[index].className.includes('ervaring')) {
-              allBoxes[index].children[i].style.fontSize = '24rem';
+            if (allBoxes[index].className.includes("ervaring")) {
+              allBoxes[index].children[i].style.fontSize = "24rem";
             }
-            if(allBoxes[index].className.includes('progervaring')){
-              allBoxes[index].children[i].style.fontSize = '21rem';
+            if (allBoxes[index].className.includes("progervaring")) {
+              allBoxes[index].children[i].style.fontSize = "21rem";
             }
-            if (allBoxes[index].className.includes('projecten')) {
-              allBoxes[index].children[i].style.fontSize = '22rem';
+            if (allBoxes[index].className.includes("projecten")) {
+              allBoxes[index].children[i].style.fontSize = "22rem";
             }
-            if (allBoxes[index].className.includes('opleiding')) {
-              allBoxes[index].children[i].style.fontSize = '20rem';
+            if (allBoxes[index].className.includes("opleiding")) {
+              allBoxes[index].children[i].style.fontSize = "20rem";
             }
-            if (allBoxes[index].className.includes('hobbys')) {
-              allBoxes[index].children[i].style.fontSize = '25rem';
+            if (allBoxes[index].className.includes("hobbys")) {
+              allBoxes[index].children[i].style.fontSize = "25rem";
             }
-            allBoxes[index].children[i].style.opacity = '20%';
-            allBoxes[index].children[i].style.position = 'absolute';
-
+            allBoxes[index].children[i].style.opacity = "20%";
+            allBoxes[index].children[i].style.position = "absolute";
           }
         }
       }
-
     }
 
     //zichtbaar maken bntExit
     zichtbaarBtnExit();
   }
 
-
   //terug naar homeScreen
   function BackToHomeScreen() {
     for (let index = 0; index < allBoxes.length; index++) {
-      if (allBoxes[index].style.width != '12rem') {
-        allBoxes[index].style.backgroundColor = 'black';
-        allBoxes[index].style.marginTop = '40vh';
-        allBoxes[index].style.marginLeft = 'auto';
-        allBoxes[index].style.marginRight = 'auto';
-        allBoxes[index].style.animation = 'animate 7s infinite';
-        allBoxes[index].style.width = '12rem';
-        allBoxes[index].style.height = '17rem';
-        allBoxes[index].style.transform = 'perspective(1000px) rotateX(35deg) rotateY(4deg) rotateZ(-30deg)';
-
+      if (allBoxes[index].style.width != "12rem") {
+        allBoxes[index].style.backgroundColor = "black";
+        allBoxes[index].style.marginTop = "40vh";
+        allBoxes[index].style.marginLeft = "auto";
+        allBoxes[index].style.marginRight = "auto";
+        allBoxes[index].style.animation = "animate 7s infinite";
+        allBoxes[index].style.width = "12rem";
+        allBoxes[index].style.height = "17rem";
+        allBoxes[index].style.transform =
+          "perspective(1000px) rotateX(35deg) rotateY(4deg) rotateZ(-30deg)";
       }
-      allBoxes[index].style.display = 'flex';
+      allBoxes[index].style.display = "flex";
 
       for (let i = 0; i < allBoxes[index].children.length; i++) {
-        if (allBoxes[index].children[i].className.includes('content')) {
-          allBoxes[index].children[i].style.display = 'none';
-
+        if (allBoxes[index].children[i].className.includes("content")) {
+          allBoxes[index].children[i].style.display = "none";
         }
-        if (allBoxes[index].children[i].tagName == 'H2') {
-          allBoxes[index].children[i].style.fontSize = '1.7rem';
-          allBoxes[index].children[i].style.opacity = '100%';
+        if (allBoxes[index].children[i].tagName == "H2") {
+          allBoxes[index].children[i].style.fontSize = "1.7rem";
+          allBoxes[index].children[i].style.opacity = "100%";
         }
-
       }
     }
   }
 
   //aanmaken exit button
   function aanmakenExitBtn() {
-    let imgExit = document.createElement('img');
-    imgExit.src = 'images/closeIcon.png';
-    let btnExit = document.createElement('div');
-    imgExit.style.padding = '0.8rem';
+    let imgExit = document.createElement("img");
+    imgExit.src = "images/closeIcon.png";
+    let btnExit = document.createElement("div");
+    imgExit.style.padding = "0.8rem";
 
-    btnExit.className = 'btnExit button';
-    imgExit.style.width = '3.5rem';
-    imgExit.style.height = '3.5rem';
-    btnExit.style.display = 'none';
-    btnExit.style.border = '1px solid black';
-    btnExit.style.borderRadius = '50rem';
-    btnExit.style.position = 'absolute';
-    btnExit.style.top = '90vh';
-    btnExit.style.right = '5vh';
-    btnExit.style.zIndex = '10';
-    btnExit.style.cursor = 'pointer';
+    btnExit.className = "btnExit button";
+    imgExit.style.width = "3.5rem";
+    imgExit.style.height = "3.5rem";
+    btnExit.style.display = "none";
+    btnExit.style.border = "1px solid black";
+    btnExit.style.borderRadius = "50rem";
+    btnExit.style.position = "absolute";
+    btnExit.style.top = "90vh";
+    btnExit.style.right = "5vh";
+    btnExit.style.zIndex = "10";
+    btnExit.style.cursor = "pointer";
     btnExit.appendChild(imgExit);
-    document.getElementsByTagName('main')[0].appendChild(btnExit);
-
+    document.getElementsByTagName("main")[0].appendChild(btnExit);
   }
 
   function zichtbaarBtnExit() {
-    document.getElementsByClassName('btnExit ')[0].style.display = 'block';
+    document.getElementsByClassName("btnExit ")[0].style.display = "block";
   }
 
   //bereken van leeftijd op een doorgegeven datum
   function calculateAge(birthDate) {
     var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
     var yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
+    today = mm + "/" + dd + "/" + yyyy;
 
     let now = new Date(today);
     let birth = new Date(birthDate);
 
-    const myAge = (now - birth) / 31557600000 /* from ms to year */ ;
+    const myAge = (now - birth) / 31557600000; /* from ms to year */
 
     procentNextAge = `${(myAge - Math.floor(myAge)) * 100}%`;
     console.log(procentNextAge);
-    document.getElementById('myBar').style.width = procentNextAge;
+    document.getElementById("myBar").style.width = procentNextAge;
 
     return myAge;
   }
 
   function hoverProgressBar(e) {
-    let tooltip = document.getElementsByClassName('tooltip')[0];
+    let tooltip = document.getElementsByClassName("tooltip")[0];
     let element = e.target;
     let x = e.clientX;
     let y = e.clientY;
 
-
-    if (element.id == 'myAgeProgress' || element.id == 'myBar') {
-      tooltip.style.top = y - 20 + window.scrollY + 'px';
-      tooltip.style.left = x + 15 + 'px';
-      tooltip.innerHTML = round(procentNextAge.replace('%', ''), 2) + '%';
-      tooltip.style.opacity = '100';
-    } else if (element.id !== 'myAgeProgress' || element.id !== 'myBar') {
-      tooltip.style.opacity = '0';
+    if (element.id == "myAgeProgress" || element.id == "myBar") {
+      tooltip.style.top = y - 20 + window.scrollY + "px";
+      tooltip.style.left = x + 15 + "px";
+      tooltip.innerHTML = round(procentNextAge.replace("%", ""), 2) + "%";
+      tooltip.style.opacity = "100";
+    } else if (element.id !== "myAgeProgress" || element.id !== "myBar") {
+      tooltip.style.opacity = "0";
     }
-
-
   }
 
   function round(value, decimals) {
-    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
   }
 })();
